f3ccd431269af459d349b7350ea241d118087e57
liweidong1216 <li16520@163.com>
2016-07-08 20:33:23 +0800

    fix bug

--- src/snippet/Snippet.java
+++ src/snippet/Snippet.java
@@ -3,11 +3,11 @@ package snippet;
 public class Snippet {
 	public static void main(String[] args) {
 		System.out.println("MyGitHubTest");
-		if(true){
-			System.out.println("this is true");
-		}
 		if(false){
 			System.out.println("this is false");
+		}
+		if(true){
+			System.out.println("this is true");
 		}
 	}
 }
