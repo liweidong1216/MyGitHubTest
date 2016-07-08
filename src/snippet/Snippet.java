cb68964e544019f9a17bdc6cb022a67336e38445
liweidong1216 <li16520@163.com>
2016-07-08 20:41:36 +0800

    1111

--- src/snippet/Snippet.java
+++ src/snippet/Snippet.java
@@ -3,9 +3,10 @@ package snippet;
 public class Snippet {
 	public static void main(String[] args) {
 		System.out.println("MyGitHubTest");
-		if(false){
-			System.out.println("this is false");
+		if(true){
+			System.out.println("this is true");
 		}
+		
 	}
 }
 
